b54d0a2e537ee2959b4806a2fecc4142c2b0a722
m-gheini <mahya.gheini78@gmail.com>
2020-12-21 12:58:04 +0330

    Phase4.Sixth Part. Modify auth.controller. Tested.

--- src/auth/auth.controler.ts
+++ src/auth/auth.controler.ts
@@ -1,6 +1,7 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get , Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller()
 export class AuthController {
@@ -10,5 +11,11 @@ export class AuthController {
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
